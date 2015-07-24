1159ffe99213c7e6c7d142cb4ffcd06481d3584e
U-zhangy-PC\zhangy <zhangy@zhangy-PC.(none)>
2015-07-24 11:25:54 +0800

    save

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function InvoiceCntl($scope){
+	$scope.qty = 1;
+	$scope.cost = 19.95;
+
+}
+
+it('should show of angular binding',function(){
+	expect(binding('qty * cost')).toEqual('$19.95');
+	input('qty').enter('2');
+	input('cost').enter('5.00');
+	expect(binding('qty * cost')).toEqual('$10.00');
+})
